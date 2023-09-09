1d012dc1ecf8a81b1c489c519b6bd6cd502d911e
Scikous <rozeusker@gmail.com>
2023-09-09 05:08:09 +0300

    Updated README, Fixed docker-compose.yml, Updated local to run on port 8888

--- e2e-playwright/tests/createErrors.spec.js
+++ e2e-playwright/tests/createErrors.spec.js
@@ -16,7 +16,7 @@ test("Topics create page has expected errors", async ({ page }) => {
     await expect(page.locator('ul li p').nth(1)).toHaveText("topic is required");
    
     //already in database        
-    await page.locator("input[type=text]").type("Finnish Language");
+    await page.locator("input[type=text]").type("Finnish language");
     await page.locator("button.btn.btn-primary:has-text('Add')").click();
     await expect(page.locator('ul li p').nth(0)).toHaveText("Topic exists already");
     
@@ -32,8 +32,10 @@ test("Question create page has expected errors", async ({ page }) => {
   await expect(page.locator('ul li p').nth(1)).toHaveText("question_text is required");
 
   //already in database
-  await page.locator("textarea").type(questionAO);
-
+  await page.locator("textarea").type("You can create made up words and still be understood fine?");
+  await page.locator("button.btn.btn-primary:has-text('Add')").click();
+  await expect(page.locator('ul li p').nth(0)).toHaveText("Question exists already");
+  
 
 });
 
@@ -42,7 +44,11 @@ test("Question answer options create page has expected errors", async ({ page })
 
   await page.locator("button.btn.btn-primary:has-text('Add')").click();
   await expect(page.locator('ul li p').nth(0)).toHaveText("option_text cannot be lower than 1 characters");
-  await expect(page.locator('ul li p').nth(1)).toHaveText("option_text is required");   
+  await expect(page.locator('ul li p').nth(1)).toHaveText("option_text is required");
+
+  await page.locator("textarea").type("Absolutely");
+  await page.locator("button.btn.btn-primary:has-text('Add')").click();
+  await expect(page.locator('ul li p').nth(0)).toHaveText("Question answer option exists already");
 });
 
 
