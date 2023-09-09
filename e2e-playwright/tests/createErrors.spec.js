4dc0e2fa58a078fcb239e88a6efd7a2dfbb478be
Scikous <rozeusker@gmail.com>
2023-09-09 05:14:44 +0300

    Updated .yml, Update cleaned up code

--- e2e-playwright/tests/createErrors.spec.js
+++ e2e-playwright/tests/createErrors.spec.js
@@ -50,30 +50,3 @@ test("Question answer options create page has expected errors", async ({ page })
   await page.locator("button.btn.btn-primary:has-text('Add')").click();
   await expect(page.locator('ul li p').nth(0)).toHaveText("Question answer option exists already");
 });
-
-
-// test("Question Answer Options Create page has expected user add and delete functionality", async ({ page }) => {
-//     await page.locator('h3 a').first().click();
-//     await expect(page).toHaveTitle("Questions Create");
-//     await page.locator('h3 a').first().click();
-//     await expect(page).toHaveTitle("Question Answer Options Create");
-
-//     const questionAOs = ['groceries are not real', 'anime is real', 'animals are not real', 'vehicles are real', 'weapons are not real', 'sweets are real', "power tools are not real"];
-
-//     for (const questionAO of questionAOs){
-//         //try to create topic
-//         await page.locator("textarea").type(questionAO);
-//         await page.locator("input[type=checkbox]").click();
-//         await page.locator("button.btn.btn-primary").click();
-//         await expect(page).toHaveTitle("Question Answer Options Create");
-//     }
-
-//     for (let i = 0; i < questionAOs.length; i++){
-//         //try to delete question answer option
-//         await page.locator("button.btn.btn-danger").nth(0).click();
-//         await expect(page).toHaveTitle("Question Answer Options Create");
-//     }
-
-//     await page.locator("button.btn.btn-danger:has-text('Delete question')").click();
-//     await expect(page).toHaveTitle("Questions Create");
-// });
