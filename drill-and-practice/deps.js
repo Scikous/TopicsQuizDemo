3a9c93627258971876f3fb9db404a50fa01127ee
Scikous <rozeusker@gmail.com>
2023-09-30 15:18:06 +0300

    Update deps.js and main.eta

--- drill-and-practice/deps.js
+++ drill-and-practice/deps.js
@@ -1,17 +1,17 @@
-export { configure, renderFile } from "https://deno.land/x/eta@v2.0.0/mod.ts";
+export { configure, renderFile } from "https://deno.land/x/eta@v2.2.0/mod.ts";
 export {
   Application,
   Router,
   send,
-} from "https://deno.land/x/oak@v11.1.0/mod.ts";
-import postgres from "https://deno.land/x/postgresjs@v3.3.3/mod.js";
+} from "https://deno.land/x/oak@v12.6.1/mod.ts";
+import postgres from "https://deno.land/x/postgresjs@v3.3.5/mod.js";
 export { postgres };
 export {
   Session,
   CookieStore,
-} from "https://deno.land/x/oak_sessions@v4.0.5/mod.ts";
+} from "https://deno.land/x/oak_sessions@v4.1.9/mod.ts";
 export * as bcrypt from "https://deno.land/x/bcrypt@v0.4.1/mod.ts";
 export * as validasaur from "https://deno.land/x/validasaur@v0.15.0/mod.ts";
 export { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 export { superoak } from "https://deno.land/x/superoak@4.7.0/mod.ts";
-export { assertEquals, assertAlmostEquals, assertMatch, assertStringIncludes } from "https://deno.land/std@0.171.0/testing/asserts.ts";
+export { assertEquals, assertAlmostEquals, assertMatch, assertStringIncludes } from "https://deno.land/std@0.202.0/testing/asserts.ts";
